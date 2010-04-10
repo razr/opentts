54dff759e4f32acba3e6ca800ee16553af33e054
Christopher Brannon <cmbrannon79@gmail.com>
2010-04-10 10:15:35 -0500

    Reorganized options.c and options.h for the "say" client.
    src/clients/say/options.h had the following problems.
    * No include guards.
    * No extern keyword on global variables.
    * Defined static variables, which should only be available in options.c.
    
    All are fixed.  I also added definitions for globals to options.c.
    
    Signed-off-by: Christopher Brannon <cmbrannon79@gmail.com>

--- src/clients/say/options.c
+++ src/clients/say/options.c
@@ -28,8 +28,53 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <getopt.h>
 #include <assert.h>
 #include "options.h"
+
+/* Definitions of global variables. */
+signed int rate;
+signed int pitch;
+signed int volume;
+
+char *output_module;
+char *language;
+char *voice_type;
+char *punctuation_mode;
+char *priority;
+int pipe_mode;
+int ssml_mode;
+int spelling;
+int wait_till_end;
+int stop_previous;
+int cancel_previous;
+
+char *application_name;
+char *connection_name;
+
+static struct option long_options[] = {
+    {"rate", 1, 0, 'r'},
+    {"pitch", 1, 0, 'p'},
+    {"volume", 1, 0, 'i'},
+    {"output-module", 1, 0, 'o'},
+    {"language", 1, 0, 'l'},
+    {"voice-type", 1, 0, 't'},
+    {"punctuation-mode", 1, 0, 'm'},
+    {"spelling", 0, 0, 's'},
+    {"ssml", 0, 0, 'x'},
+    {"pipe-mode", 0, 0, 'e'},
+    {"priority", 1, 0, 'P'},
+    {"application-name", 1, 0, 'N'},
+    {"connection-name", 1, 0, 'n'},
+    {"wait", 0, 0, 'w'},
+    {"stop", 1, 0, 'S'},
+    {"cancel", 1, 0, 'C'},
+    {"version", 0, 0, 'v'},
+    {"help", 0, 0, 'h'},
+    {0, 0, 0, 0}
+};
+
+static char* short_options = "r:p:i:l:o:t:m:sxeP:N:n:wSCvh";
 
 void
 options_print_help(char *argv[])
--- src/clients/say/options.h
+++ src/clients/say/options.h
@@ -2,6 +2,7 @@
  * options.h - Defines possible command line options
  *
  * Copyright (C) 2003 Brailcom, o.p.s.
+ * Copyright (C) 2010 OpenTTS Developers
  *
  * This is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by
@@ -18,54 +19,33 @@
  * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
  * Boston, MA 02110-1301, USA.
  *
- * $Id: options.h,v 1.10 2006-07-11 16:12:27 hanke Exp $
  */
 
-#include <getopt.h>
+#ifndef OPTIONS_H
+#define OPTIONS_H
 
-signed int rate;
-signed int pitch;
-signed int volume;
+/* Global variable declarations. */
+extern signed int rate;
+extern signed int pitch;
+extern signed int volume;
 
-char *output_module;
-char *language;
-char *voice_type;
-char *punctuation_mode;
-char *priority;
-int pipe_mode;
-int ssml_mode;
-int spelling;
-int wait_till_end;
-int stop_previous;
-int cancel_previous;
+extern char *output_module;
+extern char *language;
+extern char *voice_type;
+extern char *punctuation_mode;
+extern char *priority;
+extern int pipe_mode;
+extern int ssml_mode;
+extern int spelling;
+extern int wait_till_end;
+extern int stop_previous;
+extern int cancel_previous;
 
-char *application_name;
-char *connection_name;
+extern char *application_name;
+extern char *connection_name;
 
-static struct option long_options[] = {
-    {"rate", 1, 0, 'r'},
-    {"pitch", 1, 0, 'p'},
-    {"volume", 1, 0, 'i'},
-    {"output-module", 1, 0, 'o'},
-    {"language", 1, 0, 'l'},
-    {"voice-type", 1, 0, 't'},
-    {"punctuation-mode", 1, 0, 'm'},
-    {"spelling", 0, 0, 's'},
-    {"ssml", 0, 0, 'x'},
-    {"pipe-mode", 0, 0, 'e'},
-    {"priority", 1, 0, 'P'},
-    {"application-name", 1, 0, 'N'},
-    {"connection-name", 1, 0, 'n'},
-    {"wait", 0, 0, 'w'},
-    {"stop", 1, 0, 'S'},
-    {"cancel", 1, 0, 'C'},
-    {"version", 0, 0, 'v'},
-    {"help", 0, 0, 'h'},
-    {0, 0, 0, 0}
-};
-
-static char* short_options = "r:p:i:l:o:t:m:sxeP:N:n:wSCvh";
-
+/* Function prototypes. */
 int options_parse(int argc, char *argv[]);
 void options_print_version();
 void options_print_help(char *argv[]);
+#endif
