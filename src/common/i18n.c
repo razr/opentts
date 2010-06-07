ec5afb06cc1a1147c4558acdbbf63908f7f188d0
Rui Batista <ruiandrebatista@gmail.com>
2010-06-07 01:38:59 +0100

    Add i18n support functions to common library
    Any source file in the modules or server which needs access to the tags
    for marking strings which need to be translated should include the
    i18n.h header file.
    
    The otts_i18n_init function should be called before any messages that
    need to be translated are printed.
    
    Signed-off-by: William Hubbs <w.d.hubbs@gmail.com>

---
+++ src/common/i18n.c
@@ -0,0 +1,34 @@
+/*
+ * i18n.c - i18n support for OpenTTS
+ *
+ * Copyright (C) 2010 OpenTTS Developers
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this package; see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ *
+ */
+
+#if HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include "i18n.h"
+
+void otts_init_i18n(void)
+{
+	setlocale(LC_ALL, "");
+	bindtextdomain(GETTEXT_PACKAGE, LOCALEDIR);
+	textdomain(GETTEXT_PACKAGE);
+}
---
+++ src/common/i18n.h
@@ -0,0 +1,35 @@
+/*
+ * i18n.h - i18n support for OpenTTS
+ *
+ * Copyright (C) 2010 OpenTTS Developers
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this package; see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ *
+ */
+
+#ifndef OTTS_I18N
+#define OTTS_I18N 1
+#include <glib/gi18n.h>
+
+/*
+* Inits i18n support for OpenTTS components
+* This function must be called before any component outputs messages
+* that should be translated to the user.
+*/
+
+void otts_i18n_init(void);
+
+#endif // OTTS_I18N
