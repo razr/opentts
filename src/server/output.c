razr/opentts

commit dce3d0f463038ce0307b792b13125326f0932cba

use OTTS_MsgSettings in TFDSetElement
This patch was edited by Chris Brannon, to also rename SPDMsgSettings
to OTTS_MsgSettings.

Signed-off-by: Christopher Brannon <cmbrannon79@gmail.com>

src/server/output.c
@@ -368,8 +368,8 @@ SPDVoice **output_list_voices(char *module_name)
     }else{ \
        g_string_append_printf(set_str, #name"=NULL\n"); \
     }
-#define ADD_SET_STR_C(name, fconv) \
-    val = fconv(msg->settings.name); \
+#define ADD_SET_STR_C(name, var, fconv) \
+    val = fconv(msg->settings.msg_settings.var); \
     if (val != NULL){ \
        g_string_append_printf(set_str, #name"=%s\n", val); \
     }else{ \
@@ -385,17 +385,30 @@ int output_send_settings(openttsd_message * msg, OutputModule * output)
 
 	MSG(4, "Module set parameters.");
 	set_str = g_string_new("");
-	ADD_SET_INT(pitch);
-	ADD_SET_INT(rate);
-	ADD_SET_INT(volume);
-	ADD_SET_STR_C(punctuation_mode, punct2str);
-	ADD_SET_STR_C(spelling_mode, spell2str);
-	ADD_SET_STR_C(cap_let_recogn, recogn2str);
-	ADD_SET_STR(language);
-	ADD_SET_STR_C(voice, voice2str);
-	ADD_SET_STR(synthesis_voice)
+	g_string_append_printf(set_str, "pitch=%d\n",
+	                       msg->settings.msg_settings.pitch);
+	g_string_append_printf(set_str, "rate=%d\n",
+	                       msg->settings.msg_settings.rate);
+	g_string_append_printf(set_str, "volume=%d\n",
+	                       msg->settings.msg_settings.volume);
+	ADD_SET_STR_C(punctuation_mode, punctuation_mode, punct2str);
+	ADD_SET_STR_C(spelling_mode, spelling_mode, spell2str);
+	ADD_SET_STR_C(cap_let_recogn, cap_let_recogn, recogn2str);
+	ADD_SET_STR_C(voice, voice_type, voice2str);
+	if (GlobalFDSet.msg_settings.voice.language != NULL) {
+		g_string_append_printf(set_str, "language=%s\n",
+		                       GlobalFDSet.msg_settings.voice.language);
+	} else {
+		g_string_append_printf(set_str, "language=NULL\n");
+	}
+	if (GlobalFDSet.msg_settings.voice.name != NULL) {
+		g_string_append_printf(set_str, "synthesis_voice=%s\n",
+		                       GlobalFDSet.msg_settings.voice.language);
+	} else {
+		g_string_append_printf(set_str, "synthesis_voice=NULL\n");
+	}
 
-	    SEND_CMD_N("SET");
+	SEND_CMD_N("SET");
 	SEND_DATA_N(set_str->str);
 	SEND_CMD_N(".");
 

doc/opentts.texi
@@ -2893,7 +2893,7 @@ in order for us to be in a better legal position against possible intruders.
 @subsection The Skeleton of an Output Module 
 
 Each output module should include @file{src/modules/module_utils.h} where the
-SPDMsgSettings structure is defined to be able to handle the different
+OTTS_MsgSettings structure is defined to be able to handle the different
 speech synthesis settings.
 
 @example
@@ -3117,7 +3117,7 @@ differently, if the synthesizer supports it).
 
 Each output module should take care of setting the output device to
 the parameters from msg_settings (defined in module_utils.h) (See
-SPDMsgSettings in @file{src/modules/module_utils.h}). However, it is not an error if
+OTTS_MsgSettings in @file{src/modules/module_utils.h}). However, it is not an error if
 some of these values are ignored. At least rate, pitch and language
 should be set correctly.
 
@@ -3130,7 +3130,7 @@ the name of the language according to RFC 1776 (en, cs, fr, ...). If the
 requested language is not supported by this synthesizer, it's ok to abort
 and return 0, because that's an error in user settings.
 
-Other parameters are defined in @code{SPDMsgSettings} in @file{src/modules/module_utils.h}.
+Other parameters are defined in @code{OTTS_MsgSettings} in @file{src/modules/module_utils.h}.
 
 An easy way to set the parameters is using the UPDATE_PARAMETER() and
 UPDATE_STRING_PARAMETER() macros. @xref{Module Utils Functions and

include/def.h
@@ -25,6 +25,8 @@
 
 #define OPENTTSD_DEFAULT_PORT 6560
 
+#include "opentts/opentts_types.h"
+
 typedef enum {
 	SPD_MSGTYPE_TEXT = 0,
 	SPD_MSGTYPE_SOUND_ICON = 1,
@@ -32,5 +34,18 @@ typedef enum {
 	SPD_MSGTYPE_KEY = 3,
 	SPD_MSGTYPE_SPELL = 99
 } SPDMessageType;
+
+typedef struct {
+	signed int rate;
+	signed int pitch;
+	signed int volume;
+
+	SPDPunctuation punctuation_mode;
+	SPDSpelling spelling_mode;
+	SPDCapitalLetters cap_let_recogn;
+
+	SPDVoiceType voice_type;
+	SPDVoice voice;
+} OTTS_MsgSettings;
 
 #endif

src/modules/module_main.c
@@ -41,8 +41,8 @@ AudioID *module_audio_id;
 
 int current_index_mark;
 
-SPDMsgSettings msg_settings;
-SPDMsgSettings msg_settings_old;
+OTTS_MsgSettings msg_settings;
+OTTS_MsgSettings msg_settings_old;
 
 int Debug;
 FILE *CustomDebugFile;

src/modules/module_utils.h
@@ -50,23 +50,10 @@
 #include <timestamp.h>
 #include "audio.h"
 
-typedef struct {
-	signed int rate;
-	signed int pitch;
-	signed int volume;
-
-	SPDPunctuation punctuation_mode;
-	SPDSpelling spelling_mode;
-	SPDCapitalLetters cap_let_recogn;
-
-	SPDVoiceType voice_type;
-	SPDVoice voice;
-} SPDMsgSettings;
-
 extern AudioID *module_audio_id;
 
-extern SPDMsgSettings msg_settings;
-extern SPDMsgSettings msg_settings_old;
+extern OTTS_MsgSettings msg_settings;
+extern OTTS_MsgSettings msg_settings_old;
 
 extern int Debug;
 extern FILE *CustomDebugFile;

src/server/alloc.c
@@ -60,8 +60,10 @@ openttsd_message *copy_message(openttsd_message * old)
 
 	new->settings = old->settings;
 
-	new->settings.language = g_strdup(old->settings.language);
-	new->settings.synthesis_voice = g_strdup(old->settings.synthesis_voice);
+	new->settings.msg_settings.voice.language =
+		g_strdup(old->settings.msg_settings.voice.language);
+	new->settings.msg_settings.voice.name =
+		g_strdup(old->settings.msg_settings.voice.name);
 	new->settings.client_name = g_strdup(old->settings.client_name);
 	new->settings.output_module = g_strdup(old->settings.output_module);
 	new->settings.index_mark = g_strdup(old->settings.index_mark);
@@ -74,8 +76,8 @@ void mem_free_fdset(TFDSetElement * fdset)
 	/* Don't forget that only these items are filled in
 	   in a openttsd_message */
 	g_free(fdset->client_name);
-	g_free(fdset->language);
-	g_free(fdset->synthesis_voice);
+	g_free(fdset->msg_settings.voice.language);
+	g_free(fdset->msg_settings.voice.name);
 	g_free(fdset->output_module);
 	g_free(fdset->index_mark);
 }

src/server/configuration.c
@@ -164,7 +164,7 @@ void free_config_options(configoption_t * opts, int *num)
 
 /* == CALLBACK DEFINITIONS == */
 GLOBAL_FDSET_OPTION_CB_STR(DefaultModule, output_module)
-GLOBAL_FDSET_OPTION_CB_STR(DefaultLanguage, language)
+GLOBAL_FDSET_OPTION_CB_STR(DefaultLanguage, msg_settings.voice.language)
 GLOBAL_FDSET_OPTION_CB_STR(DefaultClientName, client_name)
 
 GLOBAL_FDSET_OPTION_CB_STR(AudioOutputMethod, audio_output_method)
@@ -174,13 +174,13 @@ GLOBAL_FDSET_OPTION_CB_STR(AudioNASServer, audio_nas_server)
 GLOBAL_FDSET_OPTION_CB_STR(AudioPulseServer, audio_pulse_server)
 GLOBAL_FDSET_OPTION_CB_INT(AudioPulseMinLength, audio_pulse_min_length, 1, "")
 
-GLOBAL_FDSET_OPTION_CB_INT(DefaultRate, rate, (val >= -100)
+GLOBAL_FDSET_OPTION_CB_INT(DefaultRate, msg_settings.rate, (val >= -100)
 			   && (val <= +100), "Rate out of range.")
-GLOBAL_FDSET_OPTION_CB_INT(DefaultPitch, pitch, (val >= -100)
+GLOBAL_FDSET_OPTION_CB_INT(DefaultPitch, msg_settings.pitch, (val >= -100)
 			   && (val <= +100), "Pitch out of range.")
-GLOBAL_FDSET_OPTION_CB_INT(DefaultVolume, volume, (val >= -100)
+GLOBAL_FDSET_OPTION_CB_INT(DefaultVolume, msg_settings.volume, (val >= -100)
 			   && (val <= +100), "Volume out of range.")
-GLOBAL_FDSET_OPTION_CB_INT(DefaultSpelling, spelling_mode, 1,
+GLOBAL_FDSET_OPTION_CB_INT(DefaultSpelling, msg_settings.spelling_mode, 1,
 			   "Invalid spelling mode")
 GLOBAL_FDSET_OPTION_CB_INT(DefaultPauseContext, pause_context, 1, "")
 
@@ -214,9 +214,9 @@ DOTCONF_CB(cb_DefaultCapLetRecognition)
 	g_free(errmsg);
 	g_free(val_str);
 	if (!cl_spec_section)
-		GlobalFDSet.cap_let_recogn = val;
+		GlobalFDSet.msg_settings.cap_let_recogn = val;
 	else
-		cl_spec_section->val.cap_let_recogn = val;
+		cl_spec_section->val.msg_settings.cap_let_recogn = val;
 	return NULL;
 }
 
@@ -236,9 +236,9 @@ DOTCONF_CB(cb_DefaultPunctuationMode)
 	g_free(errmsg);
 	g_free(val_str);
 	if (!cl_spec_section)
-		GlobalFDSet.punctuation_mode = val;
+		GlobalFDSet.msg_settings.punctuation_mode = val;
 	else
-		cl_spec_section->val.punctuation_mode = val;
+		cl_spec_section->val.msg_settings.punctuation_mode = val;
 	return NULL;
 }
 
@@ -258,9 +258,9 @@ DOTCONF_CB(cb_DefaultVoiceType)
 	g_free(errmsg);
 	g_free(val_str);
 	if (!cl_spec_section)
-		GlobalFDSet.voice = val;
+		GlobalFDSet.msg_settings.voice_type = val;
 	else
-		cl_spec_section->val.voice = val;
+		cl_spec_section->val.msg_settings.voice_type = val;
 	return NULL;
 }
 
@@ -425,16 +425,16 @@ DOTCONF_CB(cb_BeginClient)
 	MSG(3, "Reading configuration for pattern %s", cl_spec->pattern);
 
 	/*  Warning: If you modify this, you must also modify update_cl_settings() in set.c ! */
-	SET_PAR(rate, -101)
-	    SET_PAR(pitch, -101)
-	    SET_PAR(volume, -101)
-	    SET_PAR(punctuation_mode, -1)
-	    SET_PAR(spelling_mode, -1)
-	    SET_PAR(voice, -1)
-	    SET_PAR(cap_let_recogn, -1)
+	SET_PAR(msg_settings.rate, -101)
+	    SET_PAR(msg_settings.pitch, -101)
+	    SET_PAR(msg_settings.volume, -101)
+	    SET_PAR(msg_settings.punctuation_mode, -1)
+	    SET_PAR(msg_settings.spelling_mode, -1)
+	    SET_PAR(msg_settings.voice_type, -1)
+	    SET_PAR(msg_settings.cap_let_recogn, -1)
 	    SET_PAR(pause_context, -1);
 	SET_PAR(ssml_mode, -1);
-	SET_PAR_STR(language)
+	SET_PAR_STR(msg_settings.voice.language)
 	    SET_PAR_STR(output_module)
 
 	    return NULL;
@@ -521,16 +521,16 @@ configoption_t *load_config_options(int *num_options)
 void load_default_global_set_options()
 {
 	GlobalFDSet.priority = SPD_MESSAGE;
-	GlobalFDSet.punctuation_mode = SPD_PUNCT_NONE;
-	GlobalFDSet.spelling_mode = SPD_SPELL_OFF;
-	GlobalFDSet.rate = 0;
-	GlobalFDSet.pitch = 0;
-	GlobalFDSet.volume = 0;
+	GlobalFDSet.msg_settings.punctuation_mode = SPD_PUNCT_NONE;
+	GlobalFDSet.msg_settings.spelling_mode = SPD_SPELL_OFF;
+	GlobalFDSet.msg_settings.rate = 0;
+	GlobalFDSet.msg_settings.pitch = 0;
+	GlobalFDSet.msg_settings.volume = 0;
 	GlobalFDSet.client_name = g_strdup("unknown:unknown:unknown");
-	GlobalFDSet.language = g_strdup("en");
+	GlobalFDSet.msg_settings.voice.language = g_strdup("en");
 	GlobalFDSet.output_module = NULL;
-	GlobalFDSet.voice = SPD_MALE1;
-	GlobalFDSet.cap_let_recogn = SPD_CAP_NONE;
+	GlobalFDSet.msg_settings.voice_type = SPD_MALE1;
+	GlobalFDSet.msg_settings.cap_let_recogn = SPD_CAP_NONE;
 	GlobalFDSet.min_delay_progress = 2000;
 	GlobalFDSet.pause_context = 0;
 	GlobalFDSet.ssml_mode = SPD_DATA_TEXT;

src/server/fdset.h
@@ -41,20 +41,10 @@ typedef struct {
 	SPDMessageType type;	/* Type of the message (1=text, 2=icon, 3=char, 4=key) */
 	SPDDataMode ssml_mode;	/* SSML mode on (1)/off (0) */
 	SPDPriority priority;	/* Priority between 1 and 3 (1 - highest, 3 - lowest) */
-	signed int rate;	/* Speed of voice from <-100;+100>, 0 is the default */
-	signed int pitch;	/* Pitch of voice from <-100;+100>, 0 is the default */
-	signed int volume;	/* Volume of voice from <-100;+100), 0 is the default */
-	SPDPunctuation punctuation_mode;	/* Punctuation mode: 0, 1 or 2
-					   0    -       no punctuation
-					   1    -       all punctuation
-					   2    -       only user-selected punctuation */
-	SPDSpelling spelling_mode;	/* Spelling mode: 0 or 1 (0 - off, 1 - on) */
+	OTTS_MsgSettings msg_settings;
+
 	char *client_name;	/* Name of the client. */
-	char *language;		/* Selected language name. (e.g. "en", "cz", "fr", ...) */
 	char *output_module;	/* Output module name. (e.g. "festival", "flite", "apollo", ...) */
-	SPDVoiceType voice;
-	char *synthesis_voice;
-	SPDCapitalLetters cap_let_recogn;	/* Capital letters recognition: (0 - off, 1 - on) */
 
 	SPDNotification notification;	/* Notification about start and stop of messages, about reached
 					   index marks and state (canceled, paused, resumed). */

commit ee21a529795420cbd59364f6be8366a4bd3f935f

Fix a regression.
Commit dce3d0f463038ce0307b792b13125326f0932cba
introduced a bug.  The language and synthesis voice were being taken
from the global settings.  They should be taken from the settings for
the current message.
Also, the wrong field of the voice structure was being used when
setting the synthesis voice.

Fixed indentation, as well.

src/server/output.c
@@ -386,24 +386,25 @@ int output_send_settings(openttsd_message * msg, OutputModule * output)
 	MSG(4, "Module set parameters.");
 	set_str = g_string_new("");
 	g_string_append_printf(set_str, "pitch=%d\n",
-	                       msg->settings.msg_settings.pitch);
+			       msg->settings.msg_settings.pitch);
 	g_string_append_printf(set_str, "rate=%d\n",
-	                       msg->settings.msg_settings.rate);
+			       msg->settings.msg_settings.rate);
 	g_string_append_printf(set_str, "volume=%d\n",
-	                       msg->settings.msg_settings.volume);
+			       msg->settings.msg_settings.volume);
 	ADD_SET_STR_C(punctuation_mode, punctuation_mode, punct2str);
 	ADD_SET_STR_C(spelling_mode, spelling_mode, spell2str);
 	ADD_SET_STR_C(cap_let_recogn, cap_let_recogn, recogn2str);
 	ADD_SET_STR_C(voice, voice_type, voice2str);
-	if (GlobalFDSet.msg_settings.voice.language != NULL) {
+	if (msg->settings.msg_settings.voice.language != NULL) {
 		g_string_append_printf(set_str, "language=%s\n",
-		                       GlobalFDSet.msg_settings.voice.language);
+				       msg->settings.msg_settings.
+				       voice.language);
 	} else {
 		g_string_append_printf(set_str, "language=NULL\n");
 	}
-	if (GlobalFDSet.msg_settings.voice.name != NULL) {
+	if (msg->settings.msg_settings.voice.name != NULL) {
 		g_string_append_printf(set_str, "synthesis_voice=%s\n",
-		                       GlobalFDSet.msg_settings.voice.language);
+				       msg->settings.msg_settings.voice.name);
 	} else {
 		g_string_append_printf(set_str, "synthesis_voice=NULL\n");
 	}