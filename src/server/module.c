27b465e41d40a1c9ea665a5e31d792ada7b90d88
Trevor Saunders <trev.saunders@gmail.com>
2010-06-22 23:53:37 -0400

    Refactoring and bug-fixes in src/server/module.c.
    * We don't need to check if exec failed in module.c if it returns an
    error has taken place, so we can just log the error and exit.
    
    * break load_output_module up with helper functions
    - start_module after fork sets up file descriptors and execs the
    appropriate module
    - send_initial_commands send the initial init audio log_level commands
    to the new module
    - We create a create_module function which does the reverse of the
    destroy_module function, given parameters it allocates and copies the
    needed strings to create the module struct.
    
    * if either pipe() call at line 88 of src/server/module.c failed we
    would fail to create the module, and return NULL, but we didn't free the
    memory allocated for that modules struct.  This is fixed by calling
    destroy_module() if pipe() fails.
    
    * two variables in the load_output_module function are not needed
    - cfg this is just a flag which duplicates wheither or not we have a
    config file name
    - arg1 passed to exec as the config file name, since it was strduped it
      complicated the memory management and added nothing since it was
    equivelent to module->configfilename
    
    * rename the int fr variable that was the return from fork to the more
    descriptive pid
    * change the switch to a if, else if, else
    * make sure all negative returns from fork are considered failures
    * change a printf to use MSG
    
    * if fork failed in load_output_module() we returned null but didn't
    free the memory associated with the module's struct.  Fix this by
    calling destroy_module if fork fails.
    
    *  log if waitpid returns an error, or returns a incorrect pid, and
    reindent module.c
    * turn down the importance of a log message from warning to notice
    
    * if a module failed to communicate on startup it wasn'tkilled,
    and the data associated with it wasn't freed.  Call output_close to do
    this.
    
    Note from Chris: this commit was originally a series of 13 patches.
    
    Signed-off-by: Christopher Brannon <cmbrannon79@gmail.com>

--- src/server/module.c
+++ src/server/module.c
@@ -37,6 +37,9 @@
 #include "openttsd.h"
 #include "output.h"
 
+static OutputModule *send_initial_commands(OutputModule * module);
+static void start_module(OutputModule * module);
+
 void destroy_module(OutputModule * module)
 {
 	g_free(module->name);
@@ -45,33 +48,35 @@ void destroy_module(OutputModule * module)
 	g_free(module);
 }
 
+static OutputModule *create_module(char *name, char *prog, char *cfg_file,
+				   char *dbg_file)
+{
+	OutputModule *module;
+	char *module_conf_dir;
+
+	module = g_malloc(sizeof(OutputModule));
+	module->name = g_strdup(name);
+	module->filename = get_path(prog, MODULEBINDIR);
+	module->debugfilename = g_strdup(dbg_file);
+	module_conf_dir = g_strdup_printf("%s/modules/", options.conf_dir);
+	module->configfilename = get_path(cfg_file, module_conf_dir);
+	g_free(module_conf_dir);
+
+	return module;
+}
+
 OutputModule *load_output_module(char *mod_name, char *mod_prog,
 				 char *mod_cfgfile, char *mod_dbgfile)
 {
 	OutputModule *module;
-	int cfg = 0, ret, fr;
-	char s, *arg1 = NULL, *rep_line = NULL, *module_conf_dir;
-	FILE *f;
-	size_t n = 0;
-	GString *reply;
+	int pid;
+	int ret;
+	int status;
 
 	if (mod_name == NULL)
 		return NULL;
 
-	module = (OutputModule *) g_malloc(sizeof(OutputModule));
-
-	module->name = (char *)g_strdup(mod_name);
-	module->filename = get_path(mod_prog, MODULEBINDIR);
-
-	module_conf_dir = g_strdup_printf("%s/modules/", options.conf_dir);
-
-	module->configfilename = (char *)get_path(mod_cfgfile, module_conf_dir);
-	g_free(module_conf_dir);
-
-	if (mod_dbgfile != NULL)
-		module->debugfilename = g_strdup(mod_dbgfile);
-	else
-		module->debugfilename = NULL;
+	module = create_module(mod_name, mod_prog, mod_cfgfile, mod_dbgfile);
 
 	if (!strcmp(mod_name, "testing")) {
 		module->pipe_in[1] = 1;	/* redirect to stdin */
@@ -82,12 +87,8 @@ OutputModule *load_output_module(char *mod_name, char *mod_prog,
 	if ((pipe(module->pipe_in) != 0)
 	    || (pipe(module->pipe_out) != 0)) {
 		log_msg(OTTS_LOG_NOTICE, "Can't open pipe! Module not loaded.");
+		destroy_module(module);
 		return NULL;
-	}
-
-	if (mod_cfgfile) {
-		arg1 = g_strdup_printf("%s", module->configfilename);
-		cfg = 1;
 	}
 
 	/* Open the file for child stderr (logging) redirection */
@@ -114,166 +115,186 @@ OutputModule *load_output_module(char *mod_name, char *mod_prog,
 		log_msg(OTTS_LOG_WARN,
 			"Output module is logging to standard error output (stderr)");
 
-	fr = fork();
-	if (fr == -1) {
-		printf("Can't fork, error! Module not loaded.");
+	pid = fork();
+	if (pid < 0) {
+		log_msg(OTTS_LOG_CRIT, "Can't fork, error! Module not loaded.");
+		destroy_module(module);
 		return NULL;
-	}
-
-	if (fr == 0) {
-		ret = dup2(module->pipe_in[0], 0);
+	} else if (pid == 0) {
+/* this function never returns it either execs or exits */
+		start_module(module);
+	} else {
+		module->pid = pid;
 		close(module->pipe_in[0]);
-		close(module->pipe_in[1]);
-
-		ret = dup2(module->pipe_out[1], 1);
 		close(module->pipe_out[1]);
-		close(module->pipe_out[0]);
-
-		/* Redirrect stderr to the appropriate logfile */
-		if (module->stderr_redirect >= 0) {
-			ret = dup2(module->stderr_redirect, 2);
-		}
-
-		if (cfg == 0) {
-			if (execlp(module->filename, "", (char *)0) == -1) {
-				exit(1);
-			}
-		} else {
-			if (execlp(module->filename, "", arg1, (char *)0) == -1) {
-				exit(1);
-			}
-		}
-		assert(0);
-	}
-
-	if (cfg)
-		g_free(arg1);
-
-	module->pid = fr;
-	close(module->pipe_in[0]);
-	close(module->pipe_out[1]);
 
 	usleep(100);		/* So that the other child has at least time to fail
 				   with the execlp */
-	ret = waitpid(module->pid, NULL, WNOHANG);
-	if (ret != 0) {
-		log_msg(OTTS_LOG_WARN,
-			"ERROR: Can't load output module %s with binary %s. "
-			"Bad filename in configuration?", module->name,
-			module->filename);
-		destroy_module(module);
-		return NULL;
+		ret = waitpid(module->pid, &status, WNOHANG);
+		if (ret > 0) {
+			if (ret != module->pid)
+				log_msg(OTTS_LOG_CRIT,
+				    "error: waitpid returned a different pid, assuming module is dead (warning!) waitpid returned %d module->pid is %d",
+				    ret, module->pid);
+			log_msg(OTTS_LOG_WARN,
+			    "ERROR: Can't load output module %s with binary %s. Bad filename in configuration?",
+			    module->name, module->filename);
+			destroy_module(module);
+			return NULL;
+		} else if (ret < 0) {
+			log_msg(OTTS_LOG_CRIT, "waitpid returned an error", strerror(errno));
+		}
+
+		module->working = 1;
+		log_msg(OTTS_LOG_NOTICE, "Module %s loaded.", module->name);
+
+		/* Create a stream from the socket */
+		module->stream_out = fdopen(module->pipe_out[0], "r");
+		if (!module->stream_out)
+			FATAL
+			    ("Can't create a stream for socket, fdopen() failed.");
+		/* Switch to line buffering mode */
+		ret = setvbuf(module->stream_out, NULL, _IONBF, 4096);
+		if (ret)
+			FATAL("Can't set line buffering, setvbuf failed.");
+		return send_initial_commands(module);
 	}
 
-	module->working = 1;
-	log_msg(OTTS_LOG_WARN, "Module %s loaded.", module->name);
+	exit(1);
+}
 
-	/* Create a stream from the socket */
-	module->stream_out = fdopen(module->pipe_out[0], "r");
-	if (!module->stream_out)
-		FATAL("Can't create a stream for socket, fdopen() failed.");
-
-	/* Switch to line buffering mode */
-	ret = setvbuf(module->stream_out, NULL, _IONBF, 4096);
-	if (ret)
-		FATAL("Can't set line buffering, setvbuf failed.");
+static OutputModule *send_initial_commands(OutputModule * module)
+{
+	int ret;
 
 	log_msg(OTTS_LOG_INFO, "Trying to initialize %s.", module->name);
 	if (output_send_data("INIT\n", module, 0) != 0) {
-		log_msg(OTTS_LOG_ERR,
-			"ERROR: Something wrong with %s, can't initialize",
-			module->name);
+		log_msg(OTTS_LOG_CRIT,
+		    "ERROR: Something wrong with %s, can't initialize",
+		    module->name);
 		output_close(module);
 		return NULL;
-	}
+	} else {
+		char *rep_line = NULL;
+		FILE *f;
+		size_t n = 0;
+		char s;
+		GString *reply;
 
-	reply = g_string_new("\n---------------\n");
-	f = fdopen(dup(module->pipe_out[0]), "r");
-	while (1) {
-		ret = otts_getline(&rep_line, &n, f);
-		if (ret <= 0) {
-			log_msg(OTTS_LOG_ERR,
-				"ERROR: Bad syntax from output module %s 1",
-				module->name);
-			if (rep_line != NULL)
+		reply = g_string_new("\n---------------\n");
+		f = fdopen(dup(module->pipe_out[0]), "r");
+		while (1) {
+			ret = otts_getline(&rep_line, &n, f);
+			if (ret <= 0) {
+				log_msg(OTTS_LOG_CRIT,
+				    "ERROR: Bad syntax from output module %s 1",
+				    module->name);
+output_close(module);
+				if (rep_line != NULL)
+					free(rep_line);
+				return NULL;
+			}
+			assert(rep_line != NULL);
+			log_msg(OTTS_LOG_DEBUG, "Reply from output module: %d %s", n, rep_line);
+			if (strlen(rep_line) <= 4) {
+				log_msg(OTTS_LOG_CRIT,
+				    "ERROR: Bad syntax from output module %s 2",
+				    module->name);
 				free(rep_line);
+output_close(module);
+				return NULL;
+			}
+
+			if (rep_line[3] == '-')
+				g_string_append(reply, rep_line + 4);
+			else {
+				s = rep_line[0];
+				free(rep_line);
+				break;
+			}
+
+		}
+
+		if (options.debug) {
+			log_msg(OTTS_LOG_INFO,
+			    "Switching debugging on for output module %s",
+			    module->name);
+			output_module_debug(module);
+		}
+		/* Initialize audio settings */
+		ret = output_send_audio_settings(module);
+		if (ret != 0) {
+			log_msg(OTTS_LOG_CRIT,
+			    "ERROR: Can't initialize audio in output module, see reason above.");
+			module->working = 0;
+			kill(module->pid, 9);
+			waitpid(module->pid, NULL, WNOHANG);
+			destroy_module(module);
 			return NULL;
 		}
 
-		assert(rep_line != NULL);
-		log_msg(OTTS_LOG_DEBUG, "Reply from output module: %d %s", n,
-			rep_line);
-		if (strlen(rep_line) <= 4) {
-			log_msg(OTTS_LOG_ERR,
-				"ERROR: Bad syntax from output module %s 2",
-				module->name);
-			free(rep_line);
+		/* Send log level configuration setting */
+		ret = output_send_loglevel_setting(module);
+		if (ret != 0) {
+			log_msg(OTTS_LOG_WARN,
+			    "ERROR: Can't set the log level inin the output module.");
+			module->working = 0;
+			kill(module->pid, 9);
+			waitpid(module->pid, NULL, WNOHANG);
+			destroy_module(module);
 			return NULL;
 		}
 
-		if (rep_line[3] == '-')
-			g_string_append(reply, rep_line + 4);
-		else {
-			s = rep_line[0];
-			free(rep_line);
-			break;
+		/* Get a list of supported voices */
+		_output_get_voices(module);
+		fclose(f);
+		g_string_append_printf(reply, "---------------\n");
+
+		if (s == '2')
+			log_msg(OTTS_LOG_WARN,
+			    "Module %s started sucessfully with message: %s",
+			    module->name, reply->str);
+		else if (s == '3') {
+			log_msg(OTTS_LOG_CRIT,
+			    "ERROR: Module %s failed to initialize. Reason: %s",
+			    module->name, reply->str);
+			module->working = 0;
+			kill(module->pid, 9);
+			waitpid(module->pid, NULL, WNOHANG);
+			destroy_module(module);
+			return NULL;
 		}
-
+		g_string_free(reply, 1);
 	}
-
-	g_string_append_printf(reply, "---------------\n");
-	if (s == '2')
-		log_msg(OTTS_LOG_WARN,
-			"Module %s started sucessfully with message: %s",
-			module->name, reply->str);
-	else if (s == '3') {
-		log_msg(OTTS_LOG_ERR,
-			"ERROR: Module %s failed to initialize. Reason: %s",
-			module->name, reply->str);
-		module->working = 0;
-		kill(module->pid, 9);
-		waitpid(module->pid, NULL, WNOHANG);
-		destroy_module(module);
-		return NULL;
-	}
-	g_string_free(reply, 1);
-
-	if (options.debug) {
-		log_msg(OTTS_LOG_INFO,
-			"Switching debugging on for output module %s",
-			module->name);
-		output_module_debug(module);
-	}
-
-	/* Initialize audio settings */
-	ret = output_send_audio_settings(module);
-	if (ret != 0) {
-		log_msg(OTTS_LOG_ERR,
-			"ERROR: Can't initialize audio in output module, see reason above.");
-		module->working = 0;
-		kill(module->pid, 9);
-		waitpid(module->pid, NULL, WNOHANG);
-		destroy_module(module);
-		return NULL;
-	}
-
-	/* Send log level configuration setting */
-	ret = output_send_loglevel_setting(module);
-	if (ret != 0) {
-		log_msg(OTTS_LOG_ERR,
-			"ERROR: Can't set the log level inin the output module.");
-		module->working = 0;
-		kill(module->pid, 9);
-		waitpid(module->pid, NULL, WNOHANG);
-		destroy_module(module);
-		return NULL;
-	}
-
-	/* Get a list of supported voices */
-	_output_get_voices(module);
-	fclose(f);
 
 	return module;
+}
+
+static void start_module(OutputModule * module)
+{
+	int ret;
+
+	ret = dup2(module->pipe_in[0], 0);
+	close(module->pipe_in[0]);
+	close(module->pipe_in[1]);
+
+	ret = dup2(module->pipe_out[1], 1);
+	close(module->pipe_out[1]);
+	close(module->pipe_out[0]);
+
+	/* Redirrect stderr to the appropriate logfile */
+	if (module->stderr_redirect >= 0) {
+		ret = dup2(module->stderr_redirect, 2);
+	}
+
+	if (!(module->configfilename)) {
+		execlp(module->filename, "", (char *)0);
+	} else {
+		//execlp("valgrind", "" ,"--trace-children=yes", module->filename, arg1, arg2, (char *) 0);
+		execlp(module->filename, "", module->configfilename, (char *)0);
+	}
+	log_msg(OTTS_LOG_CRIT, "failed to exec module %s!", module->filename);
+	exit(1);
 }
 
 int unload_output_module(OutputModule * module)
